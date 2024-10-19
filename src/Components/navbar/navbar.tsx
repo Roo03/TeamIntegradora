0048d6b560e76f5d04df08c43fa0c739e9d7cc42
Roo03 <144763464+Roo03@users.noreply.github.com>
2024-10-18 21:31:30 -0600

    next update

---
+++ src/Components/navbar/navbar.tsx
@@ -0,0 +1,5 @@
+export default function NavbarPage() {
+    return (
+    <nav>Navbar content</nav>
+    );
+  }
---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import NavbarPage from "@/Components/navbar/navbar";
+import Footer from "@/Components/footer/footer";
+
+export default function Home() {
+  return (
+    <div>
+     <NavbarPage />
+        
+      
+      <Footer />
+     </div>
+  );
+}
